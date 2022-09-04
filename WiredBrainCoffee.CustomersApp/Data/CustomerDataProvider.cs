de8da6a4fde1756be29b17aea019bfb9ce042843
RyanP <tupn1702@gmail.com>
2022-09-04 10:27:45 +1000

    Adding ViewModel

---
+++ WiredBrainCoffee.CustomersApp/Data/CustomerDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.Data
+{
+    public class CustomerDataProvider : ICustomerDataProvider
+    {
+        /// <summary>
+        /// async and await is to implement Asynchronous Programming
+        /// By using Asynchronous programming, the Application can continue with the other work 
+        /// that does not depend on the completion of the entire task
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Customer>?> GetAllAsync()
+        {
+            await Task.Delay(100);//Demonstration server working time
+            return new List<Customer>
+            {
+                new Customer{Id = 1, FirstName ="Julie", LastName ="Nguyen", IsDeveloper = true},
+                new Customer{Id = 2, FirstName ="John", LastName ="Pham" },
+                new Customer{Id = 3, FirstName ="Ryan", LastName ="Pham", IsDeveloper = true},
+                new Customer{Id = 4, FirstName ="Peter", LastName ="Holman", IsDeveloper = true},
+                new Customer{Id = 5, FirstName ="Daniel", LastName ="Le"},
+                new Customer{Id = 6, FirstName ="Mick", LastName ="CQT"},
+            };
+        }
+    }
+}
---
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomerViewModel
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+        
+
+        public CustomerViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+
+        /// <summary>
+        /// ObservableCollection raised a CollectionChanged event 
+        /// when add or remove customers from the collection
+        /// </summary>
+        public ObservableCollection<Customer> Customers { get; } = new();
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any())
+            {
+                return;
+            }
+
+
+            var customers = await _customerDataProvider.GetAllAsync();
+            if (customers != null)
+            {
+                foreach (var c in customers)
+                {
+                    Customers.Add(c);
+                }
+            }
+        }
+    }
+}
